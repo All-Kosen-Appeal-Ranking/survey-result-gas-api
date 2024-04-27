355aebf21239c32f277354dfff1c94a180e9f2a1
hengin-eer <hirokix0608@gmail.com>
2024-04-28 03:00:19 +0900

    :bug: fix: オブジェクトのキーの修正、又これに伴うコードの変更
    オブジェクトのキーを日本語でのフォームラベルから、半角英数字に置き替えた

--- src/AllSurveyResult.js
+++ src/AllSurveyResult.js
@@ -5,24 +5,52 @@ function deliverAllSurveyResult() {
   let maxColNum = 202;
 
   let selectedData = usingSheet.getDataRange().getValues();
-  let rangedData = selectedData.slice(0, maxColNum);
+  let rangedData = selectedData.slice(1, maxColNum); // 0行目ラベルの除外
   
   let parsedData = [];
-  // row_i = 1の理由: 0行目のラベルを除外するため
-  for(let row_i=2-1; row_i<maxColNum; row_i++) {
+  // 0行目ラベルでデータ数が1つ少ないため、maxColNum-1
+  for(let row_i=0; row_i<(maxColNum-1); row_i++) {
     let answerDataPerPerson = {};
 
     for(let col_j=0; col_j<28; col_j++) {
       // console.log(rangedData[row_i][col_j]); // 1セルごとのデータ取得を確認
-      let labelCellData = rangedData[0][col_j];
+      let labelCellData = [
+        "timeStamp",
+        "studentGrade",
+        "school",
+        "sexRatio",
+        "professorInterest",
+        "trandition",
+        "beautySchool",
+        "goodFacilities",
+        "tasteCafeteriaFood",
+        "workingRenovation",
+        "learnAssist",
+        "remedialTraining",
+        "numRetainedStudent",
+        "clubActibity",
+        "playingSports",
+        "awardsReceived",
+        "communityAttractive",
+        "communityProsperity",
+        "easeGoSchool",
+        "storeLocationSchool",
+        "beautyDormitory",
+        "tasteDormitoryFood",
+        "storeLocationDormitory",
+        "friendshipDormitory",
+        "strictHierarchy",
+        "fullDormitoryEvents",
+        "kosenAttractiveness",
+        "kosenYouKnow",
+      ]
       let cellData = rangedData[row_i][col_j];
       
       // ブランケット記法よりキーに変数の値を使用
-      answerDataPerPerson[labelCellData] = cellData;
+      answerDataPerPerson[labelCellData[col_j]] = cellData;
     }
 
-    // 0行目ラベル処理よりループ内の初期値が1であるため、配列の0番スタート補正を掛けている
-    parsedData[row_i-1] = answerDataPerPerson;
+    parsedData[row_i] = answerDataPerPerson;
   }
 
   console.log(parsedData);
